715d2d3e582e449a0ce1b3e74b859491369199f5
donna-kdm <kadamdona@gmail.com>
2023-11-09 22:13:31 +0900

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import vitelogo from'/vite.svg'
+import './App.css'
+import { Navbar } from './assets/components/navbar'
+
+
+function App() {
+
+  return (
+    <div>
+      <Navbar/>
+    </div>
+      
+    
+  )
+}
+
+export default App
---
+++ src/assets/components/Home.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+function Home() {
+  return (
+      <div> hai Wibu </div>
+    
+  )
+}
+
+export default Home
